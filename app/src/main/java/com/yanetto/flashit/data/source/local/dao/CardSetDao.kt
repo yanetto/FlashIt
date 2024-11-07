57323a3d6a59f2bcc25cbdb496bd93eef3635066
yanetto <89203732802es@gmail.com>
2024-11-07 13:39:04 +0300

    - added annotation

--- app/src/main/java/com/yanetto/flashit/data/source/local/dao/CardSetDao.kt
+++ app/src/main/java/com/yanetto/flashit/data/source/local/dao/CardSetDao.kt
@@ -1,5 +1,6 @@
 package com.yanetto.flashit.data.source.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
@@ -7,6 +8,7 @@ import androidx.room.Update
 import com.yanetto.flashit.data.source.local.model.CardSetEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface CardSetDao {
     @Insert
     fun insertCardSet(cardSet: CardSetEntity)
