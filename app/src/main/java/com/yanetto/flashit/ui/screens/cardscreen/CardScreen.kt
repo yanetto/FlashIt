568cf4c164a8e6a2ada5a3fe1c42a86e800f2613
yanetto <89203732802es@gmail.com>
2024-11-07 16:57:16 +0300

    - added rotation card animation

--- app/src/main/java/com/yanetto/flashit/ui/screens/cardscreen/CardScreen.kt
+++ app/src/main/java/com/yanetto/flashit/ui/screens/cardscreen/CardScreen.kt
@@ -1,10 +1,13 @@
 package com.yanetto.flashit.ui.screens.cardscreen
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,16 +16,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,8 +42,10 @@ import com.yanetto.flashit.R
 
 @Composable
 fun CardScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: CardScreenViewModel = CardScreenViewModel()
 ) {
+    val uiState = viewModel.uiState.collectAsState()
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -62,32 +74,7 @@ fun CardScreen(
 
         Spacer(modifier = Modifier.size(16.dp))
 
-        Card(
-            shape = MaterialTheme.shapes.medium,
-            colors = CardColors(
-                containerColor = Color(168, 153, 255),
-                contentColor = Color.White,
-                disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.5f),
-                disabledContentColor = Color.White.copy(0.5f)
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(8f)
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Extension-функции в Kotlin",
-                    color = Color.Black,
-                    style = MaterialTheme.typography.headlineLarge,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(16.dp),
-                    fontWeight = FontWeight.Bold
-                )
-            }
-        }
+        RotatingCard(viewModel = viewModel)
 
         Spacer(modifier = Modifier.size(32.dp))
 
@@ -102,12 +89,14 @@ fun CardScreen(
                 modifier = Modifier
                     .clip(CircleShape)
                     .border(1.dp, Color.Black, CircleShape)
-                    .clickable {  }
+                    .clickable { }
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.dont_know),
                     contentDescription = null,
-                    modifier = Modifier.size(50.dp).padding(4.dp)
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(4.dp)
                 )
             }
 
@@ -117,12 +106,14 @@ fun CardScreen(
                 modifier = Modifier
                     .clip(CircleShape)
                     .border(1.dp, Color.Black, CircleShape)
-                    .clickable {  }
+                    .clickable { }
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.know),
                     contentDescription = null,
-                    modifier = Modifier.size(50.dp).padding(4.dp)
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(4.dp)
                 )
             }
 
@@ -132,7 +123,7 @@ fun CardScreen(
         Spacer(modifier = Modifier.size(32.dp))
 
         Text(
-            text = "Нажмите на карточку, чтобы перевернуть ее",
+            text = stringResource(R.string.tap_on_card_to_flip_it),
             color = Color.Black.copy(0.4f),
             fontSize = 14.sp,
             modifier = Modifier
@@ -142,6 +133,72 @@ fun CardScreen(
         )
     }
 }
+
+const val ANIMATION_DURATION = 600
+const val FULL_TURN = 360
+const val HALF_TURN = 180
+const val DENSITY_MULTIPLIER = 12
+
+@Composable
+fun ColumnScope.RotatingCard (
+    viewModel: CardScreenViewModel
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    var rotationAngle by remember { mutableStateOf(0f) }
+
+    val rotation by animateFloatAsState(
+        targetValue = rotationAngle,
+        animationSpec = tween(durationMillis = ANIMATION_DURATION),
+        label = "RotateAnimation"
+    )
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = Color(168, 153, 255),
+            contentColor = Color.White
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(8f)
+            .graphicsLayer {
+                rotationY = rotation % FULL_TURN
+                cameraDistance =
+                    DENSITY_MULTIPLIER * density
+            }
+            .clickable { rotationAngle += HALF_TURN }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    scaleX =
+                        if ((rotation % FULL_TURN) > HALF_TURN * 0.5f && (rotation % FULL_TURN) < FULL_TURN * 0.75f) -1f
+                        else 1f
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            if ((rotation % FULL_TURN) < HALF_TURN * 0.5f || (rotation % FULL_TURN) >= FULL_TURN * 0.75f) {
+                Text(
+                    text = uiState.value.question,
+                    color = Color.Black,
+                    style = MaterialTheme.typography.headlineLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+            } else {
+                Text(
+                    text = uiState.value.answer,
+                    color = Color.Black,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/yanetto/flashit/ui/screens/cardscreen/CardScreenUiState.kt
+++ app/src/main/java/com/yanetto/flashit/ui/screens/cardscreen/CardScreenUiState.kt
@@ -1,5 +1,11 @@
 package com.yanetto.flashit.ui.screens.cardscreen
 
 data class CardScreenUiState (
-    val name: String
+    val question: String = "Extension-функции в Kotlin",
+    val answer: String = "Extension-функции в Kotlin позволяют " +
+            "добавлять новые функции к существующим " +
+            "классам без изменения их кода. Эти функции выглядят как методы класса, но определяются вне его. Для создания extension-функции нужно объявить функцию с именем класса перед именем функции, например:\n" +
+            "fun String.firstChar(): Char = this[0]\nТеперь firstChar можно использовать на любом объекте типа String как обычный метод: \n" +
+            "val text = \"Hello\" println(text.firstChar()) // Выведет 'H' \n" +
+            "Extension-функции полезны для добавления функционала к классам, доступ к коду которых ограничен."
 )
